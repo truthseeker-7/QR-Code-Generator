4fd98719ddc1f1203dd0c89f6a3e2cbd624986e2
Zeyede <pawloszeyede21@augustana.edu>
2023-02-21 13:49:08 -0600

    QR code generation now working

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ generateBtn.onclick = function(){
 let containerCode = document.querySelector(".container-code");
 containerCode.classList.toggle("active"); 
 let codeImg = document.querySelector(".code-img");
-codeImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Coded By Pawlos Million Zeyede";
+
+let text = document.getElementById("url-input").value;
+
+codeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${text}`;
 codeImg.classList.toggle("active"); 
 }
