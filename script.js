bb52485fe3e3806f0c6cce9a7afce67332bd367c
Zeyede <pawloszeyede21@augustana.edu>
2023-02-21 13:30:58 -0600

    Added png image for testing transition

--- script.js
+++ script.js
@@ -3,4 +3,6 @@ let generateBtn = document.getElementById("generate");
 generateBtn.onclick = function(){
 let containerCode = document.querySelector(".container-code");
 containerCode.classList.toggle("active"); 
+let codeImg = document.querySelector(".code-img");
+codeImg.classList.toggle("active"); 
 }
