add92f5c4fe0020c041b4857b43eb6af8d1f65af
Zeyede <pawloszeyede21@augustana.edu>
2023-02-21 12:59:48 -0600

    Added some css styling

---
+++ script.js
@@ -0,0 +1,6 @@
+let generateBtn = document.getElementById("generate");
+
+generateBtn.onclick = function(){
+let containerCode = document.querySelector(".container-code");
+containerCode.classList.toggle("active"); 
+}
