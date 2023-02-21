24e975b84950dad325053a03fd270bcc859cdcb0
Zeyede <pawloszeyede21@augustana.edu>
2023-02-21 13:41:53 -0600

    Now using QR code API to get qr code

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ generateBtn.onclick = function(){
 let containerCode = document.querySelector(".container-code");
 containerCode.classList.toggle("active"); 
 let codeImg = document.querySelector(".code-img");
+codeImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Coded By Pawlos Million Zeyede";
 codeImg.classList.toggle("active"); 
 }
