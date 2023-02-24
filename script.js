e86a4c12e08d29a1dd3689b8263d5c0098c45083
P.Million <pawloszeyede21@augustana.edu>
2023-02-23 19:32:37 -0600

    Fixed dropdown error

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let generateBtn = document.getElementById("generate");
 
-generateBtn.onclick = function(){
+generateBtn.addEventListener("click", () => {
 let containerCode = document.querySelector(".container-code");
-containerCode.classList.toggle("active"); 
+containerCode.classList.add("active"); 
 let codeImg = document.querySelector(".code-img");
 
 let text = document.getElementById("url-input").value;
 
 codeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${text}`;
-codeImg.classList.toggle("active"); 
-}
+codeImg.addEventListener("load", () => {
+    codeImg.classList.add("active"); 
+    generateBtn.innerText = "Generating QR Code..."
+})
+});
