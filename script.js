ef45532f888a27073fed3fad6ad01089df7d981d
P.Million <pawloszeyede21@augustana.edu>
2023-02-23 21:40:15 -0600

    Added default qr code and improved styling

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 let generateBtn = document.getElementById("generate");
+let codeImg = document.querySelector(".code-img");
+let containerCode = document.querySelector(".container-code");
 
 generateBtn.addEventListener("click", () => {
-let containerCode = document.querySelector(".container-code");
-containerCode.classList.add("active"); 
-let codeImg = document.querySelector(".code-img");
-
-let text = document.getElementById("url-input").value;
+    let text = document.getElementById("url-input").value;
+    if(text == ''){
+        codeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Coded By Pawlos Million Zeyede`;
+        containerCode.classList.add("active"); 
+        codeImg.classList.add("active"); 
+        return;
+    }
 
 codeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${text}`;
+containerCode.classList.add("active"); 
 codeImg.addEventListener("load", () => {
     codeImg.classList.add("active"); 
-    generateBtn.innerText = "Generating QR Code..."
 })
 });
